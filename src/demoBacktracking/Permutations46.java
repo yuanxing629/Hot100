515946e31bafa444877fb76fe1fd2b45f79f0ec2
YuanXing <1129294076@qq.com>
2026-03-11 14:46:15 +0800

    回溯

---
+++ src/demoBacktracking/Permutations46.java
@@ -0,0 +1,46 @@
+package demoBacktracking;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
+ */
+public class Permutations46 {
+
+    List<List<Integer>> result = new ArrayList<>();
+    LinkedList<Integer> path = new LinkedList<>();
+    boolean[] used;
+
+    public List<List<Integer>> permute(int[] nums) {
+        if (nums.length == 0) {
+            return result;
+        }
+        used = new boolean[nums.length];
+        backtrack(nums);
+        return result;
+    }
+
+    private void backtrack(int[] nums) {
+        if (path.size() == nums.length) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) {
+                continue;
+            }
+
+            used[i] = true;
+            path.add(nums[i]);
+
+            backtrack(nums);
+
+            path.removeLast();
+            used[i] = false;
+        }
+    }
+
+
+}
